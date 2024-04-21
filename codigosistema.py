79e82060e171bf67829112d79a6bbe04e052ab8b
walison <franciscooficialpro@gmail.com>
2024-04-21 18:06:25 -0300

    Commit de informações iniciais

---
+++ codigosistema.py
@@ -0,0 +1,14 @@
+# inicio do projeto + possíveis dados que iremos usar
+          #sistema de senhas para clientes que chegam na agência
+
+# 1) Dados de entrada 
+
+
+# 2) Processamento dos dados 
+
+
+
+# 3) Saida (O que irá ser executado e mostrado)
+
+
+
